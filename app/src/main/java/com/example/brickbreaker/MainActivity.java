efae6c0609573b73b95c8ba22b13548b98b2aefa
ayush.trivedi0511@gmail.com <its.trivedi05@h=gmail.com>
2023-05-08 10:17:41 +0530

    f1

---
+++ app/src/main/java/com/example/brickbreaker/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.brickbreaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int b;
+        setContentView(R.layout.activity_main);
+    }
+}
